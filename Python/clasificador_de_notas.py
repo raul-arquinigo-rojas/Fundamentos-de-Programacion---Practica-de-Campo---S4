a5da6d2e4db654963cc47601c74ea23183bd5511
RaulAr <raula@staarbot.com>
2026-05-01 23:50:47 -0500

    Elimado archivos inncesarios

--- Python/clasificador_de_notas.py
+++
@@ -1,9 +0,0 @@
-print('Ingrese la nota del estudiante: ')
-nota = float(input())
-if nota >= 11:
-    if nota == 20:
-        print('Excelente')
-    else:
-        print('Aprobado')
-else:
-    print('Desaprobado')
